db82fd2cb55b6175348e30ece74451672c5d0ffc
Poppy Tait <poppytait@gmail.com>
2021-06-12 10:27:26 +0200

    Task 1

--- interview-eventmanager/src/test/java/com/strands/interviews/eventsystem/DefaultEventManagerTest.java
+++ interview-eventmanager/src/test/java/com/strands/interviews/eventsystem/DefaultEventManagerTest.java
@@ -36,6 +36,15 @@ public class DefaultEventManagerTest
     }
 
     @Test
+    public void testListenerWithoutMatchingPublishedEventClass()
+    {
+        EventListenerMock eventListenerMock = new EventListenerMock(new Class[]{SimpleEvent.class});
+        eventManager.registerListener("some.key", eventListenerMock);
+        eventManager.publishEvent(new SubEvent(this));
+        assertFalse(eventListenerMock.isCalled());
+    }
+
+    @Test
     public void testUnregisterListener()
     {
         EventListenerMock eventListenerMock = new EventListenerMock(new Class[]{SimpleEvent.class});
